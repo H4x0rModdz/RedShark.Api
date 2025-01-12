39f55316690eb0f6a5de771835be308fb3b574b4
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-01-12 15:44:39 -0300

    Add Logo and Validate User Name w/ Your Format

--- RedShark.Domain/Exceptions/UserInvalidNameException.cs
+++ RedShark.Domain/Exceptions/UserInvalidNameException.cs
@@ -4,8 +4,18 @@ namespace RedShark.Domain.Exceptions
 {
     public class UserInvalidNameException : PublicException
     {
+        public UserInvalidNameException(string message) : base(message)
+        {
+        }
+
         public UserInvalidNameException() : base("Name is required.")
         {
         }
+
+        public static UserInvalidNameException InvalidFormat() =>
+            new UserInvalidNameException("Name contains invalid characters.");
+
+        public static UserInvalidNameException NullOrWhitespace() =>
+            new UserInvalidNameException("Name cannot be null or whitespace.");
     }
 }
