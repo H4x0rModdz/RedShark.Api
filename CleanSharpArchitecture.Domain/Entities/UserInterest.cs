4e57fb19023c5560311e604e93a4ff5d963520c4
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-02-12 20:45:04 -0300

    Create UserInterest.cs

---
+++ CleanSharpArchitecture.Domain/Entities/UserInterest.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanSharpArchitecture.Domain.Entities
+{
+    public class UserInterest : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public List<Interest> Interests { get; set; }
+    }
+}
