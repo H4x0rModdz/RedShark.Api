575155c4abca680543333dc396e2ac0698b35c7e
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-02-12 20:45:03 -0300

    Update User.cs

--- CleanSharpArchitecture.Domain/Entities/User.cs
+++ CleanSharpArchitecture.Domain/Entities/User.cs
@@ -5,6 +5,7 @@ namespace CleanSharpArchitecture.Domain.Entities
 {
     public class User : BaseEntity
     {
+        public string UserName { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
@@ -12,6 +13,10 @@ namespace CleanSharpArchitecture.Domain.Entities
         public string Biography { get; set; }
         public string? RecoveryCode { get; set; }
         public DateTime? RecoveryCodeExpiration { get; set; }
+        public bool IsLocked { get; set; }
+        public DateTime? LockedUntil { get; set; }
+        public int FailedLoginAttempts { get; private set; }
+        public DateTime? LastFailedLoginAttempt { get; private set; }
 
         // Relationships
         public ICollection<Post> Posts { get; set; } = new List<Post>();
@@ -21,6 +26,36 @@ namespace CleanSharpArchitecture.Domain.Entities
         public ICollection<Follower> Followers { get; set; } = new List<Follower>();
         public ICollection<Notification> Notifications { get; set; } = new List<Notification>();
         public ICollection<UserChat> Chats { get; set; } = new List<UserChat>();
+
+        public bool IsAccountLocked() => IsLocked && LockedUntil > DateTime.UtcNow;
+
+        public void RegisterFailedAttempt()
+        {
+            FailedLoginAttempts++;
+            LastFailedLoginAttempt = DateTime.UtcNow;
+
+            if (FailedLoginAttempts >= 5 && LastFailedLoginAttempt.Value.AddMinutes(-30) <= DateTime.UtcNow)
+                LockAccount();
+        }
+
+        private void LockAccount()
+        {
+            IsLocked = true;
+            LockedUntil = DateTime.UtcNow.AddMinutes(30);
+        }
+
+        public void ResetFailedAttempts()
+        {
+            FailedLoginAttempts = 0;
+            LastFailedLoginAttempt = null;
+        }
+
+        public void UnlockAccount()
+        {
+            IsLocked = false;
+            LockedUntil = null;
+            ResetFailedAttempts();
+        }
 
         public User()
         {
