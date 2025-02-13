3f87bc4d8d10b237f9300ff6e7657f83718eee4a
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-02-13 18:34:34 -0300

    Update Comment.cs

--- CleanSharpArchitecture.Domain/Entities/Comment.cs
+++ CleanSharpArchitecture.Domain/Entities/Comment.cs
@@ -9,5 +9,9 @@ namespace CleanSharpArchitecture.Domain.Entities
         public User User { get; set; }
         public Guid PostId { get; set; }
         public Post Post { get; set; }
+
+        public Guid? ParentCommentId { get; set; }
+        public Comment ParentComment { get; set; }
+        public ICollection<Comment> Replies { get; set; } = new List<Comment>();
     }
 }
