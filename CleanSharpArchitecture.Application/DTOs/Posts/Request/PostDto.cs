1abf999cf30011294d3ce8447d1b14c57dd25b95
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2025-02-13 18:34:26 -0300

    Update PostDto.cs

--- CleanSharpArchitecture.Application/DTOs/Posts/Request/PostDto.cs
+++ CleanSharpArchitecture.Application/DTOs/Posts/Request/PostDto.cs
@@ -1,4 +1,5 @@
-﻿using CleanSharpArchitecture.Application.DTOs.Posts.PostImages;
+﻿using CleanSharpArchitecture.Application.DTOs.Comments;
+using CleanSharpArchitecture.Application.DTOs.Posts.PostImages;
 
 namespace CleanSharpArchitecture.Application.DTOs.Posts.Request
 {
@@ -8,9 +9,21 @@ namespace CleanSharpArchitecture.Application.DTOs.Posts.Request
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+
+        // Informações do usuário que criou o post
         public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string UserImage { get; set; }
+
+        // Estatísticas do post
         public int CommentsCount { get; set; }
         public int LikesCount { get; set; }
+
+        // Imagens associadas ao post
         public IEnumerable<PostImageDto>? Images { get; set; } = new List<PostImageDto>();
+
+        // Lista de comentários do post
+        public IEnumerable<CommentDto>? Comments { get; set; } = new List<CommentDto>();
     }
 }
